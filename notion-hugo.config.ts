38475240f719f8bf127ae1c1091e3a4f93b1ed27
toutouzi <74299849+toutouzi@users.noreply.github.com>
2025-09-14 00:31:32 +0800

    notion-hugo.config.ts : change base_url && page_url

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -1,10 +1,10 @@
 import { UserConfig } from "./src/config"
 
 const userConfig: UserConfig = {
-    base_url: "https://notion-hugo.pages.dev",
+    base_url: "https://toutouzi.github.io",
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/Notion-Hugo-26d02f1da16681ee8a8dd1718339b812?source=copy_link',
         pages: [
             // {
             //     page_id: '<page_id>',
